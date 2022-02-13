77404aac437873cfb648b81b4aaef1cd3f1e39d3
= <=>
2022-02-13 11:28:24 -0700

    added some es6 config and created a function to generate a fake table

--- frontend/src/about.svelte
+++ frontend/src/about.svelte
@@ -1,15 +1,47 @@
-<script lang="ts">
-  let foo: string = "bar";
+<script>
+  import { dataset_dev } from "svelte/internal";
+  import faker from "/lib/faker.js";
+  let foo = "bar";
 
-  setInterval((): void => {
-    foo = Math.random().toString(36).replace(/[^a-z]+/g, '');
-  }, 2000);
+  const table = async () => {
+    return fetch("/table").then((response) => response.json());
+  };
+
+  const tableData = table();
+
+  setInterval(() => {
+    foo = faker.lorem(2);
+  }, 5000);
 </script>
 
 <div class="flex gap-4">
   <input type="text" bind:value={foo} />
   <h1>Hello {foo}!</h1>
 </div>
+
+{#await tableData then data}
+  <div class="m-2 mt-10 rounded-t-xl p-3">
+    <table class="w-full">
+      <thead>
+        <tr class="border-b border-b-orange-400 divide-x-2">
+          {#each data.header as header}
+            <th>{header}</th>
+          {/each}
+        </tr>
+      </thead>
+
+      <tbody class="divide-y divide-gray-300">
+        {#each data.body as row}
+          <tr class="divide-x divide-gray-300">
+            {#each row as cell}
+              <td class="pl-2">{cell}</td>
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+{/await}
 
 <svelte:head>
   <title>Svelte App</title>
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const randomWord = () => {
+  return Math.random().toString(36).slice(2);
+};
+
+export const table = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  let body = [];
+  let header = [];
+
+  for (let h = 0; h < 10; h++) {
+    header.push(randomWord());
+  }
+
+  for (let r = 0; r < 10; r++) {
+    const row = [];
+    for (let c = 0; c < 10; c++) {
+      row.push(randomWord());
+    }
+    body.push(row);
+  }
+
+  response.send({ header: header, body: body });
+});
